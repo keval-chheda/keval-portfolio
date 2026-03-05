6fffc3f0f161e60f52d30d36233c62c88b67398c
KEVAL CHHEDA <kevalchheda.kc123@gmail.com>
2026-03-05 18:11:56 +0530

    SEO optimised
    SEO optimised

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,11 @@ import sitemap from 'vite-plugin-sitemap'
 export default defineConfig({
   plugins: [
     react(),
+
     sitemap({
       hostname: 'https://kevalchheda.com',
-      routes: [
-        '/',
-        '/#about',
-        '/#skills',
-        '/#experience',
-        '/#projects',
-        '/#contact',
-      ]
+      routes: ['/']
     })
-  ],
+
+  ]
 })
